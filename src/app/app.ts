c135bdbe1d15e57c7751dad947a4ac3155143d4c
Zosin0 <lucaszoser1@gmail.com>
2025-07-26 21:20:12 -0300

    ADD: Primeira ideia de Estilização de Perfil + Lógica de Permissões

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
+import { Contato, Conexao } from './lista-contatos/contato-perfil/contato-perfil';
+import { ContatoService } from '../app/core/contato-service';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,34 @@ import { Component, signal } from '@angular/core';
   standalone: false,
   styleUrl: './app.scss'
 })
+
 export class App {
-  protected readonly title = signal('AgendaADA');
+
+  title = 'agendaada';
+
+  constructor(private contatoService: ContatoService) {}
+
+
+  meuContatoFicticio: Contato = {
+    id: 1,
+    nome: 'Lucas Zoser',
+    email: 'lucaszoser1@gmail.com',
+    telefone: '+5561999695902',
+    avatarUrl: 'https://cdn-icons-png.flaticon.com/512/709/709699.png',
+    conexoes: []
+  };
+
+  ngOnInit(): void {
+    const dadosBase = {
+      nome: this.meuContatoFicticio.nome,
+      email: this.meuContatoFicticio.email,
+      telefone: this.meuContatoFicticio.telefone,
+    };
+
+    const conexoesGeradas = this.contatoService.gerarConexoes(dadosBase);
+
+    this.meuContatoFicticio.conexoes = conexoesGeradas;
+
+
+  }
 }
