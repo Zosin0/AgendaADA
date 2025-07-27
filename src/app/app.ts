78d00e5923cbd3108f687b57d3603d005d4a1dae
Gabriel S <gcsprog@gmail.com>
2025-07-27 13:05:23 -0300

    feat: criar formulário

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,6 @@
 import { Component, signal } from '@angular/core';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { Formulario } from './lista-contatos/formulario/formulario';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,26 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('AgendaADA');
+
+  constructor(
+    private modalService: NgbModal,
+  ) { }
+
+  abrirModalDeCadastro() {
+    const modalRef: NgbModalRef = this.modalService.open(Formulario, {
+      size: 'lg',
+      centered: true,
+      backdrop: 'static',
+      keyboard: false
+    });
+
+    modalRef.componentInstance.contatoParaEditar = {
+      nome: 'Fulano',
+      celular: '(99) 99999-9999',
+      sobrenome: "Silva",
+      email: "email@email.com",
+      dataNascimento: "2000-10-31"
+
+    };
+  }
 }
--- src/app/lista-contatos/formulario/formulario.ts
+++ src/app/lista-contatos/formulario/formulario.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Contato } from '../../shared/interface';
 
 @Component({
   selector: 'app-formulario',
@@ -8,4 +12,93 @@ import { Component } from '@angular/core';
 })
 export class Formulario {
 
+  @Input() contatoParaEditar?: Contato;
+
+  constructor(
+    protected activeModal: NgbActiveModal,
+    private fb: FormBuilder,
+    private datePipe: DatePipe,
+  ) { }
+
+  formulario!: FormGroup;
+
+  ngOnInit() {
+
+    this.inicializarFormulario();
+    this.formulario.markAllAsTouched();
+
+    if (this.contatoParaEditar) {
+      this.popularFormularioComDados();
+    }
+  }
+
+  private formatarData(data: Date | null | undefined): string {
+    if (!data) {
+      return '';
+    }
+
+    const dataFormatada = this.datePipe.transform(data, 'yyyy-MM-dd');
+    return dataFormatada ?? '';
+  }
+
+  private inicializarFormulario(): void {
+    this.formulario = this.fb.group({
+      nome: [null, [Validators.required, Validators.maxLength(30), Validators.minLength(1)]],
+      sobrenome: [null, [Validators.required, Validators.maxLength(70), Validators.minLength(1)]],
+      celular: [null, [Validators.required]],
+      dataNascimento: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+    });
+  }
+
+  private popularFormularioComDados(): void {
+    if (!this.contatoParaEditar) return;
+
+    const { nome, sobrenome, celular, dataNascimento, email } = this.contatoParaEditar;
+
+    this.formulario.patchValue({
+      nome: nome ?? '',
+      sobrenome: sobrenome ?? '',
+      celular: celular ?? '',
+      dataNascimento: new Date(dataNascimento),
+      email: email ?? ''
+    });
+  }
+
+
+  definirMensagemErro(campo: string): string | undefined {
+    const control = this.formulario.get(campo);
+
+    if (!control || !control.errors) return undefined;
+
+    if (control.hasError('required')) return 'Campo obrigatório.';
+
+    if (control.hasError('email')) return 'Email inválido.';
+
+    if (control.hasError('maxlength')) {
+      const erro = control.getError('maxlength');
+      return `Máximo de ${erro.requiredLength} caracteres permitidos.`;
+    }
+
+    if (control.hasError('minlength')) {
+      const erro = control.getError('minlength');
+      return `Mínimo de ${erro.requiredLength} caracteres exigidos.`;
+    }
+
+    return undefined;
+  }
+
+  definirToolTipBtnSalvar(): string | null {
+    return !this.formulario.valid ? "O formulário está inválido." : null;
+  }
+
+  definirTituloFormulario(): string {
+    return this.contatoParaEditar ? "Editar Contato" : "Criar Contato";
+  }
+
+  enviarDados(): void {
+    console.log(this.formatarData(this.formulario.get('dataNascimento')?.value));
+    console.log(this.formulario.get('nome')?.value);
+    // this.activeModal.close();
+  }
 }
